ee233fc72ece712f1f28c658891619eebb744e6f
Mark Anthony Bagasol <markbagasol@gmail.com>
2019-06-11 02:05:13 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,5 +2,6 @@ def oxford_comma(array)
   if array.length == 2 
     puts "#{array[0]} and #{array[1]}"
   elsif array.length > 2 
-    array[-1].insert(0, and)
+    array[-1].insert(0, "and")
+    
 end
